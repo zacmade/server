93dca3a53be72596a13350252c928e5ab9d2559e
Zac Madeisky <168606098+zacmade@users.noreply.github.com>
2025-03-19 23:51:47 +1100

    Latest

--- socket.py
+++ socket.py
@@ -10,3 +10,13 @@ SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    padded_send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+
+send("Hello")
